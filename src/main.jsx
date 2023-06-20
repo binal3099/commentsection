ccd0e19e313a1794c669a3f950221604c283bb02
binal3099 <binalvarsani544@gmail.com>
2023-06-20 11:19:40 +0530

    commentsection

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CommentSection from './CommentSection/CommentSection.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CommentSection />
+  </React.StrictMode>,
+)
